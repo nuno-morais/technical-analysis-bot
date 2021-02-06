1bb05ec896d236587da34ca054286892e1fcffa5
Nuno Morais <nram@nuno-morais.eu>
2021-02-06 22:32:47 +0000

    Remove listeners from apps

--- apps/analyser/src/main.ts
+++ apps/analyser/src/main.ts
@@ -1,14 +1,7 @@
 import { NestFactory } from '@nestjs/core';
-import { MicroserviceOptions } from '@nestjs/microservices';
 import { AnalyserModule } from './analyser.module';
 
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
-    AnalyserModule,
-  );
-
-  await app.listen(() =>
-    console.log("Microservice 'SchedulerModule' is listening"),
-  );
+  await NestFactory.createApplicationContext(AnalyserModule);
 }
 bootstrap();
--- apps/scheduler/src/main.ts
+++ apps/scheduler/src/main.ts
@@ -1,14 +1,7 @@
 import { NestFactory } from '@nestjs/core';
-import { MicroserviceOptions } from '@nestjs/microservices';
 import { SchedulerModule } from './scheduler.module';
 
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
-    SchedulerModule,
-  );
-
-  await app.listen(() =>
-    console.log("Microservice 'SchedulerModule' is listening"),
-  );
+  await NestFactory.createApplicationContext(SchedulerModule);
 }
 bootstrap();
