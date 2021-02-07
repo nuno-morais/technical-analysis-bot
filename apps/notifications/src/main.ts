037a121d49a256f452a9058ebc3272491e788bbd
Nuno Morais <nram@nuno-morais.eu>
2021-02-07 18:13:08 +0000

    Support port by env var

--- apps/notifications/src/main.ts
+++ apps/notifications/src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
+import 'dotenv/config';
 import { NotificationsModule } from './notifications.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(NotificationsModule);
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
