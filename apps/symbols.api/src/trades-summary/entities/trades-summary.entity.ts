f79ab695cfe7a03de680efd3eda61d17d7e9b97d
Nuno Morais <nram@nuno-morais.eu>
2021-04-15 10:24:57 +0100

    Add id on trades summary

--- apps/symbols.api/src/trades-summary/entities/trades-summary.entity.ts
+++ apps/symbols.api/src/trades-summary/entities/trades-summary.entity.ts
@@ -1,9 +1,12 @@
 import { Trade } from '@tab/core';
 
 export class TradesSummary {
+  id: string;
   constructor(
     public trades: Trade[],
     public total: number,
     public year: number = null,
-  ) {}
+  ) {
+    this.id = (year || 'current').toString();
+  }
 }
